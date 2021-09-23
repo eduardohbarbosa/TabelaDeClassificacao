8388b9eb8b58049f8098d9715f7d10ae4f05653e
EDUARDO HENRIQUE <eduhenrique5478@gmail.com>
2021-09-23 20:02:54 -0300

    Adicionei função de adicionar/excluir um jogador

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
-var eduardo = {nome: "eduardo", vitoria: 0, empates: 0, derrotas: 0, pontos: 0}
+var listaJogadores = []
+var divAcoes = document.querySelector("#acoes")
 
-var listaJogadores = [eduardo]
+/*Verifica se o Jogador já existe na Lista*/
+function jogadorExiste(jogador){
+    for(i = 0; i < listaJogadores.length; i++){
+        if(listaJogadores[i].nome.toUpperCase().indexOf(jogador.toUpperCase()) != -1 & listaJogadores[i].nome.toUpperCase() == jogador.toUpperCase()){
+            return true
+        }
+    }
 
-function adicionarJogador(){
-    var campoJogador = document.querySelector("#acoes")
-    campoJogador.innerHTML += `<p>Nome do Jogador: <input type="txt" id="nomeJogador"></p>`
-    campoJogador.innerHTML += `<button class="btnAcao" onclick="criarJogador(nomeJogador.value)"><span>Adicionar<span></button>`
+    return false
 }
 
+/*Exibe o jogador na tela*/
+function exbirJogador(){
+    var camposTabela = ''
+    for(var i = 0; i < listaJogadores.length; i++){    
+        camposTabela += `<tr>`
+        camposTabela += `<td>${listaJogadores[i].nome}</td>`
+        camposTabela += `<td>${listaJogadores[i].vitorias}</td>`
+        camposTabela += `<td>${listaJogadores[i].empates}</td>`
+        camposTabela += `<td>${listaJogadores[i].derrotas}</td>`
+        camposTabela += `<td>${listaJogadores[i].pontos}</td>`
+        camposTabela += `<td>`
+        camposTabela += `<button onclick="adicionarVitoria()">Vitoria</button>`
+        camposTabela += `<button onclick="adicionarEmpate()">Empate</button>`
+        camposTabela += `<button onclick="adicionarDerrota()">Derrota</button>`
+        camposTabela += `</td>`
+        camposTabela += `</tr>`
+    }
+
+    var tabelaJogadores = document.querySelector("#tabelaJogadores")
+    tabelaJogadores.innerHTML = camposTabela
+}
+
+/*Cria o input e o botão de criação de um novo jogador*/
+function adicionarJogador(){
+    divAcoes.innerHTML = `<p>Nome do Jogador: <input type="txt" id="nomeJogador"></p>`
+    divAcoes.innerHTML += `<button class="btnAcao" onclick="criarJogador(nomeJogador.value)"><span>Adicionar<span></button>`
+}
+
+/*Cria o objeto do jogador com nome, numero de vitorias, empates e derrotas e seu pontos*/
 function criarJogador(jogador){
-    if(listaJogadores.indexOf(jogador) == -1){
+    var jogadorNaLista = jogadorExiste(jogador)
+
+    if(jogadorNaLista == false){
         var jogador = {
             nome: jogador, 
             vitorias: 0,
@@ -18,29 +53,37 @@ function criarJogador(jogador){
             pontos: 0
         }
 
-        var camposTabela = ''
-        camposTabela += `<tr>`
-        camposTabela += `<td>${jogador.nome}</td>`
-        camposTabela += `<td>${jogador.vitorias}</td>`
-        camposTabela += `<td>${jogador.derrotas}</td>`
-        camposTabela += `<td>${jogador.empates}</td>`
-        camposTabela += `<td>${jogador.pontos}</td>`
-        camposTabela += `<td>`
-        camposTabela += `<button onclick="adicionarVitoria()">Vitoria</button>`
-        camposTabela += `<button onclick="adicionarEmpate()">Empate</button>`
-        camposTabela += `<button onclick="adicionarDerrota()">Derrota</button>`
-        camposTabela += `</td>`
-        camposTabela += `</tr>`
-    
-        var tabelaJogadores = document.querySelector("#tabelaJogadores")
-        tabelaJogadores.innerHTML += camposTabela
-
-        return listaJogadores.push(jogador)
-    }else if(listaJogadores.indexOf(jogador) != -1 ) {
+        listaJogadores.push(jogador)
+        exbirJogador()
+    }else if(jogadorNaLista == true){
         alert("Jogador já existe")
     }
 
+    divAcoes.innerHTML = `<button class="btnAcao" onclick="adicionarJogador()"><span>Novo Jogador</span></button>`
+    divAcoes.innerHTML += `<button class="btnAcao" onclick="excluirJogador()"><span>Excluir Jogador</span></button>`
 }
 
-criarJogador("Eduardo")
-criarJogador("Ana")
+/*Cria o input e o botão de exclusão de um novo jogador*/
+function excluirJogador(){
+    divAcoes.innerHTML = `<p>Nome do Jogador: <input type="txt" id="nomeJogador"></p>`
+    divAcoes.innerHTML += `<button class="btnAcao" onclick="deletarJogador(nomeJogador.value)"><span>Adicionar<span></button>`
+}
+
+function deletarJogador(jogador){
+    var jogadorNaLista = jogadorExiste(jogador)
+
+    if(jogadorNaLista == true){
+        for (i = 0; i < listaJogadores.length; i++){
+            if(listaJogadores[i].nome.toUpperCase() == jogador.toUpperCase()){
+                listaJogadores.splice(i, 1)
+            }
+        }
+        exbirJogador()
+    }else if(jogadorNaLista == false){
+        alert("Jogador não está na tabela")
+    }
+
+
+    divAcoes.innerHTML = `<button class="btnAcao" onclick="adicionarJogador()"><span>Novo Jogador</span></button>`
+    divAcoes.innerHTML += `<button class="btnAcao" onclick="excluirJogador()"><span>Excluir Jogador</span></button>`
+}
